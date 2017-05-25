57a3041c89b04fb3c69b7d93a2a60c4fe522d41b
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-05-24 22:07:01 -0300

    Atualização de repositóeio

---
+++ BolaoNet.Reports/IApostasUserReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.Reports
+{
+    public interface IApostasUserReport
+    {
+        void GenerateApostas(Entities.Boloes.Bolao bolao, Entities.Campeonatos.Campeonato campeonato, Entities.Users.User user, 
+            IList<Entities.Campeonatos.Jogo> jogos, IList<Entities.Boloes.Bolao> jogosUsuarios);
+    }
+}
--- BolaoNet.Reports/PDF/BasePdfManager.cs
+++ BolaoNet.Reports/PDF/BasePdfManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BolaoNet.Reports.PDF
 {
-    class BasePdfManager
+    public class BasePdfManager
     {
     }
 }
