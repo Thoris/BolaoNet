ce78f19bb13a403bdf44f93209d313db2f321912
Thoris <thorisangelo@hotmail.com>
2017-05-26 15:53:23 -0300

    Adicionando arquivos de controladores de serviço REST.

---
+++ BolaoNet.Services/Areas/Boloes/Controllers/BolaoController.cs
@@ -0,0 +1,38 @@
+﻿using BolaoNet.Services.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BolaoNet.Services.Areas.Boloes.Controllers
+{
+    public class BolaoController : GenericApiController<Entities.Boloes.Bolao>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Propriedade que retorna o objeto que possui regras de negócio do gerenciamento da entidade.
+        /// </summary>
+        private Business.Interfaces.Boloes.IBolaoBO Dao
+        {
+            get { return (Business.Interfaces.Boloes.IBolaoBO)base.BaseBo; }
+        }
+
+        #endregion
+
+        #region Constructors/Destructors
+
+        public BolaoController()
+            : base ( new Business.FactoryBO().CreateBolaoBO())
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+    }
+}
---
+++ BolaoNet.Services/Areas/Boloes/Controllers/BolaoPontoRodadaController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BolaoNet.Services.Areas.Boloes.Controllers
+{
+    public class BolaoPontoRodadaController
+    {
+    }
+}
