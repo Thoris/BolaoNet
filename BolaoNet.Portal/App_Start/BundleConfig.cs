75fef33d583eb4d7c3f3c1e469c1cd156549706a
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-05-29 21:50:00 -0300

    Organização da camada de visualização.

--- BolaoNet.Portal/App_Start/BundleConfig.cs
+++ BolaoNet.Portal/App_Start/BundleConfig.cs
@@ -8,24 +8,57 @@ namespace BolaoNet.Portal
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Content/js_old/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Content/js_old/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Content/js_old/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Content/js_old/bootstrap.js",
+            //          "~/Content/js_old/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/css_old/bootstrap.css",
+            //          "~/Content/css_old/site.css"));
+
+
+
+
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Content/js_old/jquery-{version}.js"));
+                        "~/Content/vendor/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Content/js_old/jquery.validate*"));
+                        "~/Content/vendor/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Content/js_old/modernizr-*"));
+                        "~/Content/vendor/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/js_old/bootstrap.js",
-                      "~/Content/js_old/respond.js"));
+                      "~/Content/vendor/bootstrap/js/bootstrap.js",
+                      "~/Content/vendor/bootstrap/js/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/css_old/bootstrap.css",
-                      "~/Content/css_old/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                    "~/Content/vendor/bootstrap/css/bootstrap.css"
+                ));
+
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/css_old/bootstrap.css",
+            //          "~/Content/css_old/site.css"));
+
+
+
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
