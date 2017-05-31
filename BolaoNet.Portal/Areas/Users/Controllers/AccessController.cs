bf0d0c7515e1c739bf3d1072c75b9118dd33818b
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-05-30 21:44:19 -0300

    Atualização de camada de visualização para acesso ao sistema.

---
+++ BolaoNet.Portal/Areas/Users/Controllers/AccessController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BolaoNet.Portal.Areas.Users.Controllers
+{
+    public class AccessController : Controller
+    {
+        #region Actions
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+        public ActionResult RegistrationForm()
+        {
+            return View();
+        }
+        public ActionResult ActivateCode()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
