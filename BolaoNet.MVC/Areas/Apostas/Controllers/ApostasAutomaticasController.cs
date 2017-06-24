f995b36df0b5984b82f8bf26e4a64b409bc1a428
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-06-23 21:55:26 -0300

    Atualização de controlador de apostas automáticas.

---
+++ BolaoNet.MVC/Areas/Apostas/Controllers/ApostasAutomaticasController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BolaoNet.MVC.Areas.Apostas.Controllers
+{
+    public class ApostasAutomaticasController : BaseApostasController
+    {
+        #region Variables
+
+        private Application.Interfaces.Boloes.IJogoUsuarioApp _jogoUsuarioApp;
+
+        #endregion
+
+        #region Constructors/Destructors
+
+        public ApostasAutomaticasController(Application.Interfaces.Boloes.IJogoUsuarioApp jogoUsuarioApp,
+                Application.Interfaces.Campeonatos.ICampeonatoApp campeonatoApp,
+                Application.Interfaces.Campeonatos.ICampeonatoFaseApp campeonatoFaseApp,
+                Application.Interfaces.Campeonatos.ICampeonatoGrupoApp campeonatoGrupoApp,
+                Application.Interfaces.Campeonatos.ICampeonatoTimeApp campeonatoTimeApp
+            )
+            : base(campeonatoApp, campeonatoFaseApp, campeonatoGrupoApp, campeonatoTimeApp)
+        {
+            _jogoUsuarioApp = jogoUsuarioApp;
+        }
+
+        #endregion
+
+        #region Actions
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        #endregion
+    }
+}
