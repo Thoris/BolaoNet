f5d92566cbbea9cb5e33a272a07c0bcdee6453c1
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-07-07 19:06:31 -0300

    Atualização da página de imagem de perfil.

---
+++ BolaoNet.MVC.ViewModels/Users/AccountHomeProfileImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BolaoNet.MVC.ViewModels.Users
+{
+    public class AccountHomeProfileImage
+    {
+        #region Properties
+
+        public string UserName { get; set; }
+        public string TempCropFile { get; set; }
+        public string TempUploadedFile { get; set; }
+        public HttpPostedFileBase AttachedFile { get; set; }
+        public string ImageData { get; set; }
+
+        #endregion
+    }
+}
--- BolaoNet.MVC/Areas/Users/Controllers/AccountHomeProfileImageController.cs
+++ BolaoNet.MVC/Areas/Users/Controllers/AccountHomeProfileImageController.cs
@@ -37,88 +37,110 @@ namespace BolaoNet.MVC.Areas.Users.Controllers
 
             return View(model);
         }
-        
-        [HttpPost]
-        public ActionResult Upload(ViewModels.Users.AccountHomeProfileImage model)
-        {
-            if (model.AttachedFile != null && model.AttachedFile.ContentLength > 0)
-            {
-                var fileName = Path.GetFileName(model.AttachedFile.FileName);
-                
-                string fullServerFile = Path.Combine(Server.MapPath(TempUploadedFolder), 
-                    string.Format ("{0}.gif", model.UserName));
-
-                var path = fullServerFile;
-                
-                model.AttachedFile.SaveAs(path);
-
-                model.TempUploadedFile = (TempUploadedFolder) + "/" + 
-                    string.Format ("{0}.gif", model.UserName);
-            }
-
-            model.UserName = base.UserLogged.UserName;
-
-            return View("Index", model);
-        }
-        
-        [HttpPost]
-        public virtual ActionResult CropImage(string imagePath, int? cropPointX, int? cropPointY, int? imageCropWidth, int? imageCropHeight)
-        {
-            if (string.IsNullOrEmpty(imagePath)
-                || !cropPointX.HasValue
-                || !cropPointY.HasValue
-                || !imageCropWidth.HasValue
-                || !imageCropHeight.HasValue)
-            {
-                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
-            }
-
-            byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
-            byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
-
-            //string tempFolderName = Server.MapPath("~/" + ConfigurationManager.AppSettings["Image.TempFolderName"]);
-
-            string tempFolderName = Server.MapPath("~" + TempCropImageFolder);
-
-            string fileName = Path.GetFileName(imagePath);
-
-            try
-            {
-                FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
-            }
-            //catch (Exception ex)
-            catch
-            {
-                //Log an error     
-                return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
-            }
-
-            //string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
-            string photoPath = string.Concat(TempCropImageFolder, "/", fileName);
-            return Json(new { photoPath = photoPath }, JsonRequestBehavior.AllowGet);
-        }
-
         [HttpPost]
         public ActionResult Salvar(ViewModels.Users.AccountHomeProfileImage model)
         {
-            string photoPath = Server.MapPath(string.Concat(TempCropImageFolder, "/", base.UserLogged.UserName + ".gif"));
-            string targetPath = Server.MapPath (string.Concat(FolderProfileImage, "/", base.UserLogged.UserName + ".gif"));
-            string tempUploadPath = Server.MapPath(string.Concat(TempUploadedFolder, "/", base.UserLogged.UserName + ".gif"));
 
-            if (System.IO.File.Exists(targetPath))
-                System.IO.File.Delete(targetPath);
+            string source = model.ImageData;
+
+            // remove extra chars at the beginning
+            source = source.Substring(source.IndexOf(",") + 1);
+            byte[] data = Convert.FromBase64String(source);
 
 
-            System.IO.File.Copy(photoPath, targetPath);
+            string file = Server.MapPath(FolderProfileImage + "/" + base.UserLogged.UserName + ".gif");
+            if (System.IO.File.Exists (file))
+                System.IO.File.Delete (file);
+            
 
-            if (System.IO.File.Exists(photoPath))
-                System.IO.File.Delete(photoPath);
+            //Storing the data into the file
+            System.IO.File.WriteAllBytes(file, data);
 
-            if (System.IO.File.Exists(tempUploadPath))
-                System.IO.File.Delete(tempUploadPath);
 
-            return RedirectToAction("Index", "AccountHome", new { area = "Users" });
+            return RedirectToAction("Index", "AccountHome", new { area="Users" });
+            
         }
+        //[HttpPost]
+        //public ActionResult Upload(ViewModels.Users.AccountHomeProfileImage model)
+        //{
+        //    if (model.AttachedFile != null && model.AttachedFile.ContentLength > 0)
+        //    {
+        //        var fileName = Path.GetFileName(model.AttachedFile.FileName);
+                
+        //        string fullServerFile = Path.Combine(Server.MapPath(TempUploadedFolder), 
+        //            string.Format ("{0}.gif", model.UserName));
+
+        //        var path = fullServerFile;
+                
+        //        model.AttachedFile.SaveAs(path);
+
+        //        model.TempUploadedFile = (TempUploadedFolder) + "/" + 
+        //            string.Format ("{0}.gif", model.UserName);
+        //    }
+
+        //    model.UserName = base.UserLogged.UserName;
+
+        //    return View("Index", model);
+        //}
+        
+        //[HttpPost]
+        //public virtual ActionResult CropImage(string imagePath, int? cropPointX, int? cropPointY, int? imageCropWidth, int? imageCropHeight)
+        //{
+        //    if (string.IsNullOrEmpty(imagePath)
+        //        || !cropPointX.HasValue
+        //        || !cropPointY.HasValue
+        //        || !imageCropWidth.HasValue
+        //        || !imageCropHeight.HasValue)
+        //    {
+        //        return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+        //    }
+
+        //    byte[] imageBytes = System.IO.File.ReadAllBytes(Server.MapPath(imagePath));
+        //    byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX.Value, cropPointY.Value, imageCropWidth.Value, imageCropHeight.Value);
+
+        //    //string tempFolderName = Server.MapPath("~/" + ConfigurationManager.AppSettings["Image.TempFolderName"]);
+
+        //    string tempFolderName = Server.MapPath("~" + TempCropImageFolder);
+
+        //    string fileName = Path.GetFileName(imagePath);
+
+        //    try
+        //    {
+        //        FileHelper.SaveFile(croppedImage, Path.Combine(tempFolderName, fileName));
+        //    }
+        //    //catch (Exception ex)
+        //    catch
+        //    {
+        //        //Log an error     
+        //        return new HttpStatusCodeResult((int)HttpStatusCode.InternalServerError);
+        //    }
+
+        //    //string photoPath = string.Concat("/", ConfigurationManager.AppSettings["Image.TempFolderName"], "/", fileName);
+        //    string photoPath = string.Concat(TempCropImageFolder, "/", fileName);
+        //    return Json(new { photoPath = photoPath }, JsonRequestBehavior.AllowGet);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Salvar(ViewModels.Users.AccountHomeProfileImage model)
+        //{
+        //    string photoPath = Server.MapPath(string.Concat(TempCropImageFolder, "/", base.UserLogged.UserName + ".gif"));
+        //    string targetPath = Server.MapPath (string.Concat(FolderProfileImage, "/", base.UserLogged.UserName + ".gif"));
+        //    string tempUploadPath = Server.MapPath(string.Concat(TempUploadedFolder, "/", base.UserLogged.UserName + ".gif"));
+
+        //    if (System.IO.File.Exists(targetPath))
+        //        System.IO.File.Delete(targetPath);
+
+
+        //    System.IO.File.Copy(photoPath, targetPath);
+
+        //    if (System.IO.File.Exists(photoPath))
+        //        System.IO.File.Delete(photoPath);
+
+        //    if (System.IO.File.Exists(tempUploadPath))
+        //        System.IO.File.Delete(tempUploadPath);
+
+        //    return RedirectToAction("Index", "AccountHome", new { area = "Users" });
+        //}
         #endregion
     }
 }
