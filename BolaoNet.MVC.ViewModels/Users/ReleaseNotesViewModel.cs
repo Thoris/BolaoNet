32712633c206691cd23ebd37ff028e0c76794c2a
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-09-03 22:55:35 -0300

    Atualização de repositório.

---
+++ BolaoNet.MVC.ViewModels/Users/ReleaseNotesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.MVC.ViewModels.Users
+{
+    public class ReleaseNotesViewModel
+    {
+        #region Properties
+
+        public string Data { get; set; }
+
+        #endregion
+    }
+}
