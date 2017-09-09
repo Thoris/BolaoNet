3cfe47b055ce837232435180608a1462575412d4
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-09-09 10:01:32 -0300

    Inclusão de itens faltantes para histórico de ganhadores.

---
+++ BolaoNet.MVC.ViewModels/Bolao/BolaoHistoricoListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.MVC.ViewModels.Bolao
+{
+    public class BolaoHistoricoListViewModel
+    {
+        public int SelectedYear { get; set; }
+        public IList<int> ListYears { get; set; }
+
+        public IList<BolaoHistoricoViewModel> Classificacao { get; set; }
+    }
+}
---
+++ BolaoNet.MVC.ViewModels/Bolao/BolaoHistoricoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.MVC.ViewModels.Bolao
+{
+    public class BolaoHistoricoViewModel
+    {
+        #region Properties
+
+        public string NomeBolao { get; set; } 
+        public int Ano { get; set; } 
+        public int Posicao { get; set; }
+        public string UserName { get; set; }
+        public int Pontos { get; set; }
+        public int TotalEmpates { get; set; }
+        public int TotalVDE { get; set; }
+        public int TotalGolsTime1 { get; set; }
+        public int TotalGolsTime2 { get; set; }
+        public int TotalCheio { get; set; }
+        public int TotalExtras { get; set; }
+
+
+        #endregion
+    }
+}
