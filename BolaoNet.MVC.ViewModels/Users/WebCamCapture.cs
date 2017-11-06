33889463330191ba0968a5bed08ea0c755605e56
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2017-11-06 08:07:20 -0200

    Atualização de repositório.

---
+++ BolaoNet.MVC.ViewModels/Users/WebCamCapture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.MVC.ViewModels.Users
+{
+    public class WebCamCapture
+    {
+        #region Properties
+
+        /// <summary>
+        /// Propriedade que configura/retorna a largura da camera.
+        /// </summary>
+        public int CameraWidth { get; set; }
+        /// <summary>
+        /// Propriedade que configura/retorna a altura da camera.
+        /// </summary>
+        public int CameraHeight { get; set; }        
+        public string ImageData { get; set; }
+
+        #endregion    
+    }
+}
