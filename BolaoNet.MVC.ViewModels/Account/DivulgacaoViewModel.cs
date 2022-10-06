6010dc92deed1c0d26a96dde49779337e81cbe31
thoris <thoris.pivetta@tivit.com>
2022-10-05 23:26:11 -0300

    Inclusão de página de divulgação.

---
+++ BolaoNet.MVC.ViewModels/Account/DivulgacaoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BolaoNet.MVC.ViewModels.Bolao;
+using BolaoNet.MVC.ViewModels.Regras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BolaoNet.MVC.ViewModels.Account
+{
+    public class DivulgacaoViewModel
+    {
+        #region Properties
+
+        public IList<BolaoRegrasViewModel> Regras { get; set; }
+        public IList<BolaoHistoricoViewModel> Classificacao { get; set; }
+
+        #endregion
+
+        #region Constructors/Destructors
+
+        public DivulgacaoViewModel()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ BolaoNet.MVC/Controllers/DivulgacaoController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BolaoNet.MVC.Controllers
+{
+    public class DivulgacaoController : BaseController
+    {
+        #region Variables
+
+        private Application.Interfaces.Boloes.IBolaoHistoricoApp _bolaoHistoricoApp;
+        private Application.Interfaces.Boloes.IBolaoRegraApp _bolaoRegraApp;
+
+        #endregion
+
+        #region Constructors/Destructors
+
+        public DivulgacaoController(Application.Interfaces.Boloes.IBolaoHistoricoApp bolaoHistoricoApp, Application.Interfaces.Boloes.IBolaoRegraApp bolaoRegraApp)
+        {
+            _bolaoHistoricoApp = bolaoHistoricoApp;
+            _bolaoRegraApp = bolaoRegraApp;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public ActionResult Index()
+        {
+            ViewModels.Account.DivulgacaoViewModel viewModel = new ViewModels.Account.DivulgacaoViewModel();
+
+            return View(viewModel);
+        }
+
+        #endregion
+    }
+}
